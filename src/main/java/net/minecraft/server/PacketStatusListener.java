906210309a0ab5b3e1584479b11018ea02a4e962
djoveryde <djoveryde@outlook.com>
2017-08-27 15:13:23 +0100

    Update PacketStatusListener.java

--- src/main/java/net/minecraft/server/PacketStatusListener.java
+++ src/main/java/net/minecraft/server/PacketStatusListener.java
@@ -2,21 +2,21 @@
   
       public void a() {}
   
- +    // PaperSpigot start - Readability for ping MOTD fix backport
- +    private static final int WAITING = 0;
- +    private static final int PING = 1;
- +    private static final int DONE = 2;
- +    private int state = WAITING;
- +    // PaperSpigot end
- +
+     // PaperSpigot start - Readability for ping MOTD fix backport
+     private static final int WAITING = 0;
+     private static final int PING = 1;
+     private static final int DONE = 2;
+     private int state = WAITING;
+     // PaperSpigot end
+ 
       public void a(PacketStatusInStart packetstatusinstart) {
- +        // PaperSpigot start - Backport ping MOTD fix
- +        if (this.state != WAITING) {
- +            this.networkManager.close(null);
- +            return;
- +        }
- +        this.state = PING;
- +        // PaperSpigot end
+         // PaperSpigot start - Backport ping MOTD fix
+         if (this.state != WAITING) {
+             this.networkManager.close(null);
+             return;
+         }
+         this.state = PING;
+         // PaperSpigot end
           // CraftBukkit start - fire ping event
           final Object[] players = minecraftServer.getPlayerList().players.toArray();
           class ServerListPingEvent extends org.bukkit.event.server.ServerListPingEvent {
@@ -24,13 +24,13 @@
       }
   
       public void a(PacketStatusInPing packetstatusinping) {
- +        // PaperSpigot start - Backport ping MOTD fix
- +        if (this.state != PING) {
- +            this.networkManager.close(null);
- +            return;
- +        }
- +        this.state = DONE;
- +        // PaperSpigot end
+         // PaperSpigot start - Backport ping MOTD fix
+         if (this.state != PING) {
+             this.networkManager.close(null);
+             return;
+         }
+         this.state = DONE;
+         // PaperSpigot end
           this.networkManager.handle(new PacketStatusOutPong(packetstatusinping.c()), new GenericFutureListener[0]);
       }
   }
