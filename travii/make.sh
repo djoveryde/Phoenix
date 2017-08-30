dbd8722b40e2cdc8cd7fa3aced6904fe15295725
Vladislav <exiscents@gmail.com>
2017-08-30 10:02:56 +0400

    Update make.sh
    try fix travis.

--- travii/make.sh
+++ travii/make.sh
@@ -5,7 +5,7 @@ cd forge
 git checkout b3a1548cfaac77e9756168a8887270f8181487ad
 cd ..
 git pull origin master
-./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar
+./gradlew --parallel clean setupCauldron --parallel jar
 cp build/distributions/Thermos*server* $HOME/release/Thermos-1.7.10-1448-server.jar
 
 # 1492
@@ -13,7 +13,7 @@ cd forge
 git checkout c308a47124394f025bc2ba553646ecf8b6c31ba2
 cd ..
 git pull origin master
-./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar
+./gradlew --parallel clean setupCauldron --parallel jar
 cp build/distributions/Thermos*server* $HOME/release/Thermos-1.7.10-1492-server.jar
 
 # 1558
@@ -21,7 +21,7 @@ cd forge
 git checkout aa8eaf2b286e809146b7faf4e59ce801a40eab9b
 cd ..
 git pull origin master
-./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar
+./gradlew --parallel clean setupCauldron --parallel jar
 cp build/distributions/Thermos*server* $HOME/release/Thermos-1.7.10-1558-server.jar
 
 # 1614
@@ -29,7 +29,7 @@ cd forge
 git checkout c6aa04325e8a80af88755adb2339a402e7e90ebb
 cd ..
 git pull origin master
-./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar packageBundle
+./gradlew --parallel clean setupCauldron --parallel jar packageBundle
 cp build/distributions/Thermos*server* $HOME/release/Thermos-1.7.10-1614-server.jar
 cd build/distributions/
 unzip Thermos*bundle*zip
